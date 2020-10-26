50130f6c6ea05fb3a235f62c5306d95e3ecaf3aa
yushinbox@gmail.com <yushinbox@gmail.com>
2020-10-26 14:42:57 +0300

    +

---
+++ animals/для практики js/index.js
@@ -0,0 +1,16 @@
+// let div=document.querySelector("p");
+// console.log(div);
+
+// let div2=document.querySelectorAll("div");
+// console.log(div2);
+
+// four.style.background="red";
+
+// let div=document.querySelector('div');
+// let child = div.childNodes;
+// // console.log(child)
+// console.log(div.firstChild);
+
+
+
+
