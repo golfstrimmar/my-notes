33b735cef3687b7dd85d2d65b9075836b4655cc3
yushinbox@gmail.com <yushinbox@gmail.com>
2020-11-16 20:35:46 +0300

    npm run build

--- для практики js/index.js
+++ для практики js/index.js
@@ -1,13 +1,46 @@
-// let div=document.querySelector("p");
-// console.log(div);
+window.onload=function(){
+  function _(el) {
+    return document.querySelector(el);
+  }
+  let sample = _(".sample .sample-board");
 
-// let div2=document.querySelectorAll("div");
-// console.log(div2);
+  //  sample.addEventListener('click', function (event) {
 
-// four.style.background="red";
+  //  this.classList.add("board");
+  //  if(this.classList.contains("board")){
+  //    this.classList.remove("board")
+  //  };
+  //  });
 
-// let div=document.querySelector('div');
-// let child = div.childNodes;
-// // console.log(child)
-// console.log(div.firstChild);
+  
+
+  // function activeItem(){
+  // this.classList.toggle("board");
+  // }
+
+  
+//  sample.onclick =	function activeItem(){
+// this.classList.toggle("board");
+//  }		
+
+let items =document.querySelectorAll(".sample .sample-board");		
+for( let i =0; i< items.length; i++		 ){
+  items[i].onclick = activeItem;
+}
+
+function activeItem() {
+    this.classList.toggle("board");
+  };
+
+// setInterval(activeItem, 500);
+
+};
+
+// замыкания - это когда одна функция объявляется внутри другой и внутренняя функция видит все переменные внешней. в данном примере activeItem() выведет в консоль sample. а если вынести объявление функции  activeItem() за пределы функции window.onload=function(). то даст ошибку
+
+
+// function activeItem(){
+// this.classList.toggle("board");	
+// }
+
 
